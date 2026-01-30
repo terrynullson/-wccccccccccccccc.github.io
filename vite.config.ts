941415f600e8b498ccf3367dde0c4a68fd8c329f
audiogenius <audiogenius2812@gmail.com>
2026-01-31 00:28:51 +0300

    Add GitHub Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+const base = process.env.BASE_PATH ?? "/";
+
 export default defineConfig({
+  base,
   plugins: [react(), svgr()],
   test: {
     globals: true,
