0e43ca0a9dd2525e65a750ea91d8d03c5d4bfd62
terrynullson <terrynullson@gmail.com>
2026-02-26 11:59:39 +0300

    1

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AppShell from './widgets/layout/AppShell';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/global.css";
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-    const root = createRoot(rootElement);
-    root.render(<AppShell />);
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
