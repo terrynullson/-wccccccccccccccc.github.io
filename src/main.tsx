e758a0c989458334d1387c981f088ac6d3646485
terrynullson <terrynullson@gmail.com>
2026-02-26 11:13:36 +0300

    Create src/main.tsx with React 19 setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/global.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import AppShell from './widgets/layout/AppShell';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    const root = createRoot(rootElement);
+    root.render(<AppShell />);
+}
